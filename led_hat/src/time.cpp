07bbb88a9df4fc4c080d8b9d8ce06b63f15b95ee
Chase Lambert <theicfire@gmail.com>
2020-01-18 23:38:58 -0800

    Bugfix: Increase time wrapping

--- led_hat/src/time.cpp
+++ led_hat/src/time.cpp
@@ -2,10 +2,11 @@
 
 static long offset = 0;
 void Time_SetTime(unsigned long t) {
-    offset = (t % 100000) - (millis() % 100000);
+  offset = (t % 4294967295) - (millis() % 4294967295);
 }
 
+// Will wrap every 49 days
 unsigned long Time_GetTime() {
-    // Serial.print("Time is: "); Serial.println(millis() + offset);
-    return (millis() + offset) % 100000;
+  // Serial.print("Time is: "); Serial.println(millis() + offset);
+  return (millis() + offset) % 4294967295;
 }
