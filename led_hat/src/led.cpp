7712ca75a13e06f44eb28b7fe4a7d30f51a443df
Chase Lambert <theicfire@gmail.com>
2020-01-19 03:37:58 -0800

    Use merged image!

--- led_hat/src/led.cpp
+++ led_hat/src/led.cpp
@@ -5,8 +5,11 @@
 #include "color_mix.h"
 #include "friend.h"
 #include "gamma.h"
-#include "image_lion.h"
+#include "image_merge.h"
 #include "time.h"
+
+// Different for each micro
+constexpr int LED_OFFSET = 0;
 
 constexpr int NUM_COLORS = 3;
 constexpr int ANIMATION_SECONDS = 10;
@@ -24,17 +27,14 @@ static CRGB br = {0, 0, 255};
 
 constexpr float WIDTH_MULTIPLY = 8.f;
 constexpr float HEIGHT_MULTIPLY = 16.f;
-constexpr int IMAGE_HEIGHT = 300;
 constexpr int IMAGE_WIDTH = 300;
 static uint8_t led_buff1[IMAGE_WIDTH][3];
 static uint8_t led_buff2[IMAGE_WIDTH][3];
-constexpr int NUM_MOVEMENTS = 6;
+constexpr int NUM_MOVEMENTS = 7;
 constexpr int MS_PER_MOVEMENT = 60000;
-static int movements[NUM_MOVEMENTS][2] = {{0, 0},     {0, 299}, {0, 150},
-                                          {150, 150}, {150, 0}, {150, 299}};
+static int movements[NUM_MOVEMENTS][2] = {{0, 0},
+{0, 793}, {0, 400}, {150, 400}, {150, 0}, {150, 793}, {100, 300}};
 
-// Different for each micro
-constexpr int LED_OFFSET = 0;
 
 CRGB getColorBuff1(int index) {
   CRGB ret = {.r = 0, .b = 0, .g = 0};
@@ -68,24 +68,14 @@ void playImage(unsigned long time) {
   printf("movement info: %d %d, y: %f, fraction: %f, x_start: %f\n",
          movement_index, previous_movement_index, y, fraction, x_start);
 
-  // constexpr int FRAME_RATE = 1000 / 60;
-  // int loc = (time / FRAME_RATE) %
-  //           ((IMAGE_HEIGHT - 1) *
-  //            ((int)HEIGHT_MULTIPLY));  // - 1 because we want to read the
-  //            last
-  //                                      // two row of flash
-  // float y = loc / HEIGHT_MULTIPLY;
-  memcpy_P(led_buff1, image_lion[yi], IMAGE_WIDTH * 3);
-  memcpy_P(led_buff2, image_lion[yi + 1], IMAGE_WIDTH * 3);
+  memcpy_P(led_buff1, image_merge[yi], IMAGE_WIDTH * 3);
+  memcpy_P(led_buff2, image_merge[yi + 1], IMAGE_WIDTH * 3);
   // TODO check that we don't go over the width
   for (int i = 0; i < NUM_LEDS; i++) {
     int x = ((i + ((int)x_start * (int)WIDTH_MULTIPLY) + LED_OFFSET) %
              (NUM_LEDS * (int)WIDTH_MULTIPLY)) /
             WIDTH_MULTIPLY;
     float x_fraction = x_start - ((int)x_start);
-    if (i == 0) {
-      printf("x is %d, x_fraction is %f\n", x, x_fraction);
-    }
     CRGB tl = getColorBuff1(x);
     CRGB tr = getColorBuff1(x + 1);
     CRGB bl = getColorBuff2(x);
