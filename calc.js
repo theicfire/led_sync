b03dac7c7ca59d715a946d363b6efe0e9bbcf855
Chase Lambert <theicfire@gmail.com>
2020-10-17 20:02:25 -0700

    Improve the angle estimate algorithm
    Just in JS right now

--- calc.js
+++ calc.js
@@ -1,6 +1,119 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 
+class AngleEstimate {
+  COMPARE_WINDOW = 10;
+  constructor() {
+    this.start_mag = 0;
+    this.current_angle = 0;
+    this.current_index = 0;
+    this.last_smaller_data_index = 0;
+    this.last_larger_data_index = 0;
+    this.avg_mag = 0;
+
+    this.last_period = 0;
+    this.switch_index = 0;
+    this.last_angle = 0;
+  }
+
+  add_accel(x, y, z) {
+    const mag = this.get_mag(x, y, z);
+    this.avg_mag = .4 * mag + .6 * this.avg_mag;
+    if (this.current_index < 90) {
+      this.start_mag = mag;
+      this.current_index += 1;
+      return;
+    }
+
+    if (mag < this.start_mag) {
+      this.last_smaller_data_index = this.current_index;
+    } else {
+      this.last_larger_data_index = this.current_index;
+    }
+    this.recalc_angle();
+    this.current_index += 1;
+  }
+
+  recalc_angle() {
+    const before_angle = this.get_angle();
+    let smaller = false;
+    let larger = false;
+
+    if (this.last_smaller_data_index >= this.current_index - this.COMPARE_WINDOW) {
+      smaller = true;
+    }
+    if (this.last_larger_data_index >= this.current_index - this.COMPARE_WINDOW) {
+      larger = true;
+    }
+    const prev_angle = this.current_angle;
+    if (!smaller) {
+      this.current_angle = 1;
+    } else if (!larger) {
+      this.current_angle = -1;
+    }
+
+    if (this.avg_mag < 300 && prev_angle == 0) {
+      this.current_angle = 0;
+    } else if (this.avg_mag < 100) {
+      this.current_angle = 0;
+    }
+    if (prev_angle !== this.current_angle) {
+      if (this.current_angle === 0) {
+        this.switch_index = 0;
+        this.last_period = 0;
+        this.last_angle = 0;
+      } else {
+        if (this.switch_index > 0) {
+          this.last_angle = before_angle;
+          this.last_period = this.current_index - this.switch_index;
+        }
+        this.switch_index = this.current_index;
+      }
+    }
+  }
+
+  get_mag(x, y, z) {
+    return Math.sqrt(Math.pow(x, 2), Math.pow(y, 2), Math.pow(z, 2));
+  }
+
+  get_angle() {
+    const TIME_SHIFT = this.COMPARE_WINDOW;
+    const steps_past_last = this.current_index - this.switch_index;
+    if (this.last_period > 0 && this.current_angle === 1) {
+      if (steps_past_last < this.last_period / 2 - TIME_SHIFT) {
+        const rise = (1 - this.last_angle);
+        const run = (this.last_period / 2) - TIME_SHIFT;
+        const m = rise / run;
+        const b = this.last_angle;
+        return Math.min(1, m * steps_past_last + b);
+      } else {
+        const rise = -2;
+        const run = (this.last_period);
+        const m = rise / run;
+        const b = 1;
+        const steps = (steps_past_last - (this.last_period / 2 - TIME_SHIFT));
+        return Math.max(-1, m * steps + b);
+      }
+    } else if (this.last_period > 0 && this.current_angle === -1) {
+      if (steps_past_last < this.last_period / 2 - TIME_SHIFT) {
+        const rise = (-1 - this.last_angle);
+        const run = (this.last_period / 2) - TIME_SHIFT;
+        const m = rise / run;
+        const b = this.last_angle;
+        return Math.max(-1, m * steps_past_last + b);
+      } else {
+        const rise = 2;
+        const run = (this.last_period);
+        const m = rise / run;
+        const b = -1;
+        const steps = (steps_past_last - (this.last_period / 2 - TIME_SHIFT));
+        return Math.min(1, m * steps + b);
+      }
+    }
+    return 0;
+  }
+}
+
 function get_angle(accel_a, accel_b) {
       const numer = accel_a.x * accel_b.x + accel_a.y * accel_b.y + accel_a.z * accel_b.z;
       const b = Math.sqrt(Math.pow(accel_a.x, 2) + Math.pow(accel_a.y, 2) + Math.pow(accel_a.z, 2));
@@ -28,48 +141,16 @@ function beginning_avg(points) {
   return sum / 50;
 }
 
-function find_patterns(points) {
-  const COMPARE_WINDOW = 10;
-  const ZERO_WINDOW = COMPARE_WINDOW;
-  const start = beginning_avg(points);
-  let direction = 0;
-  let last_switch = 0;
-  for (let i = ZERO_WINDOW; i < points.length; i++) {
-    let below = false;
-    let above = false;
-    for (let j = 0; j < COMPARE_WINDOW; j++) {
-      if (points[i - j] < start) {
-        below = true;
-      } else {
-        above = true;
-      }
-    }
-    const prev_direction = direction;
-    if (!below && direction !== 1) {
-      last_switch = i;
-      direction = 1;
-    }
-    if (!above && direction !== -1) {
-      last_switch = i;
-      direction = -1;
-    }
-
-    let sum = 0;
-    for (let j = 0; j < ZERO_WINDOW; j++) {
-      sum += Math.pow(points[i] - points[i - j], 2);
-    }
-    // Hysterisis...
-    if (Math.sqrt(sum / ZERO_WINDOW) < 300 && prev_direction == 0) {
-      direction = 0;
-    } else if (Math.sqrt(sum / ZERO_WINDOW) < 100) {
-      direction = 0;
-    }
-    console.log(direction);
+function find_patterns(rows) {
+  const estimator = new AngleEstimate();
+  for (let i = 0; i < rows.length; i++) {
+    estimator.add_accel(rows[i].x, rows[i].y, rows[i].z);
+    console.log(estimator.get_angle());
   }
 }
 
 let first;
-const lengths = [];
+const rows = [];
 fs.createReadStream('entries1.csv')
   .pipe(csv())
   .on('data', (row) => {
@@ -79,10 +160,10 @@ fs.createReadStream('entries1.csv')
       //console.log(get_smoothed_angle(first, row));
       const length = get_length(row);
       //console.log(length);
-      lengths.push(length);
-      console.log(`{${row.x},${row.y},${row.z}},`);
+      rows.push(row);
+      //console.log(`{${row.x},${row.y},${row.z}},`);
     }
   })
   .on('end', () => {
-    //find_patterns(lengths);
+    find_patterns(rows);
   });
